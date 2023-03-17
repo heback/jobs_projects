648c85ff3ee5b43c4da2210f5cf9d77b3a256cd6
heback <heback@gmail.com>
2023-03-17 15:43:19 +0900

    2023-03-17 15:43

---
+++ counter.py
@@ -0,0 +1,29 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "Flet counter example"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+    def minus_click(e):
+        pass
+
+    def plus_click(e):
+        pass
+
+    page.add(
+        ft.Row(
+            [
+                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                txt_number,
+                ft.IconButton(ft.icons.ADD, on_click=plus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    )
+
+
+ft.app(target=main)
+
